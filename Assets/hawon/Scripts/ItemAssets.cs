68f0bae33eeef9b9a90a03c61d9e22417435606c
name0191 <hawonoj002@gmail.com>
2023-01-05 11:40:56 +0900

    inventory part1
    inventory part1

---
+++ Assets/hawon/Scripts/ItemAssets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssets : MonoBehaviour
+{
+    public static ItemAssets Instance { get; private set; }
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public Transform pfItemWorld;
+
+    public Sprite swordSprite;
+    public Sprite healthPotionSprite;
+    public Sprite coinSprite;
+}
